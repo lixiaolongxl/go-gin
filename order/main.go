08e2a0ea46645a41e4f02e73e5265936c93b6271
lixiaolongxl <lxldevtest@163.com>
2022-07-13 22:59:04 +0800

    group

--- order/main.go
+++ order/main.go
@@ -7,10 +7,17 @@ import (
 	"net/http"
 )
 
+// Todo Model
+type Todo struct {
+	ID     int    `json:"id"`
+	Title  string `json:"title"`
+	Status bool   `json:"status"`
+}
+
 func main() {
 	dsn := "root:root@tcp(127.0.0.1:3306)/gorm?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	db.AutoMigrate()
+	db.AutoMigrate(&Todo{})
 	if err != nil {
 		panic("failed to connect database")
 	}
@@ -28,27 +35,57 @@ func main() {
 	V1Group := r.Group("/v1")
 	{
 		V1Group.GET("todo", func(c *gin.Context) {
-			c.JSON(http.StatusOK, gin.H{
-				"message": "ok",
-			})
+			var todoList []Todo
+			if err := db.Find(&todoList).Error; err != nil {
+				c.JSON(http.StatusOK, gin.H{"error": err.Error()})
+			} else {
+				c.JSON(http.StatusOK, todoList)
+			}
+
 		})
-		
+
 		V1Group.POST("todo", func(c *gin.Context) {
-			c.JSON(http.StatusOK, gin.H{
-				"message": "ok",
-			})
+			var todo Todo
+			c.BindJSON(&todo)
+			if err := db.Create(&todo).Error; err != nil {
+				c.JSON(http.StatusOK, gin.H{"error": err.Error()})
+			} else {
+				c.JSON(http.StatusOK, todo)
+			}
+
+		})
+		//修改
+		V1Group.PUT("todo/:id", func(c *gin.Context) {
+			id, ok := c.Params.Get("id")
+			if !ok {
+				c.JSON(http.StatusOK, gin.H{"error": "无效的id"})
+				return
+			}
+			var todo Todo
+			if err = db.Where("id=?", id).First(&todo).Error; err != nil {
+				c.JSON(http.StatusOK, gin.H{"error": err.Error()})
+				return
+			}
+			c.BindJSON(&todo)
+			if err = db.Save(&todo).Error; err != nil {
+				c.JSON(http.StatusOK, gin.H{"error": err.Error()})
+			} else {
+				c.JSON(http.StatusOK, todo)
+			}
+
 		})
 
-		V1Group.PUT("todo", func(c *gin.Context) {
-			c.JSON(http.StatusOK, gin.H{
-				"message": "ok",
-			})
-		})
-
-		V1Group.DELETE("todo", func(c *gin.Context) {
-			c.JSON(http.StatusOK, gin.H{
-				"message": "ok",
-			})
+		V1Group.DELETE("todo/:id", func(c *gin.Context) {
+			id, ok := c.Params.Get("id")
+			if !ok {
+				c.JSON(http.StatusOK, gin.H{"error": "无效的id"})
+				return
+			}
+			if err = db.Where("id=?", id).Delete(Todo{}).Error; err != nil {
+				c.JSON(http.StatusOK, gin.H{"error": err.Error()})
+			} else {
+				c.JSON(http.StatusOK, gin.H{id: "deleted"})
+			}
 		})
 	}
 
